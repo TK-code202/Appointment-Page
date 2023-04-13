b5a3f4e949f6a86637b0abcd91f6344750269462
Tofunmi Kusimo <tofunmikusimo21@gmail.com>
2023-04-13 13:25:12 +0100

    add changes to script

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
     $("#formOne").submit(function(event) {
         
-        
         $(".content").hide();
         $(".content2").show();
-       
 
+        
         event.preventDefault();
     });
 });
